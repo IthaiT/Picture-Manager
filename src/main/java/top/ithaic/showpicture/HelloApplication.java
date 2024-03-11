b93cb0885b062a3164ac4ec06f805431eea4da55
Jack299q <1625621154@qq.com>
2024-03-11 11:28:26 +0800

    Merge branch 'main' of github.com:IthaiT/Picture-Manager into main

--- src/main/java/top/ithaic/showpicture/HelloApplication.java
+++ src/main/java/top/ithaic/showpicture/HelloApplication.java
@@ -3,6 +3,7 @@ package top.ithaic.showpicture;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Button button = new Button("test");
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
