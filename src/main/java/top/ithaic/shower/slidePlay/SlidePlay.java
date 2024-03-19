ab7c99dda14ea2e80eadffaa131725df57fa07f7
Ithai <3496823588@qq.com>
2024-03-19 19:23:33 +0800

    添加幻灯片放映基本框架

--- src/main/java/top/ithaic/shower/slidePlay/SlidePlay.java
+++ src/main/java/top/ithaic/shower/slidePlay/SlidePlay.java
@@ -1,6 +1,8 @@
 package top.ithaic.shower.slidePlay;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,48 +18,36 @@ import javafx.util.Duration;
 import top.ithaic.imageview.Thumbnail;
 import top.ithaic.shower.PictureShower;
 
+import java.io.File;
+import java.util.ArrayList;
+
 
 public final class SlidePlay {
     private static Stage playStage;
     private static Scene playScene;
+    private static AnchorPane mainPane;
+    private static StackPane subPane;
+    private static ImageView imageView;
     private static Button exitButton;
 
     public SlidePlay() {
     }
     private static void initStyle(){
         playStage = new Stage();
-        playStage.initStyle(StageStyle.TRANSPARENT); // Hide window decorations
-        playStage.setFullScreen(true); // Fullscreen mode
+        mainPane = new AnchorPane();
+        subPane = new StackPane();
+        imageView = new ImageView();
         exitButton = new Button("Exit");
+        playScene = new Scene(mainPane);
+
         exitButton.setLayoutX(1375);
         exitButton.setLayoutY(800);
         exitButton.setPrefHeight(100);
         exitButton.setPrefWidth(250);
         exitButton.setFont(Font.font(30));
-    }
 
-    //TODO 为主窗口设计,供外部调用
-    public static void playPicture(){
-        initStyle();
-        FlowPane flowPane = PictureShower.getThumbnails();
-        for(Node node :flowPane.getChildren()){
-            Thumbnail thumbnail = (Thumbnail) node;
-            playPicture(thumbnail);
-        }
-
-    }
-    private static void playPicture(Thumbnail thumbnail){
-
-        Image image = new Image(thumbnail.getImageFile().toString());
-        ImageView imageView = new ImageView(image);
-        AnchorPane anchorPane = new AnchorPane();
-        anchorPane.getChildren().add(imageView);
-
-
-        StackPane buttonGroup = new StackPane();
-        buttonGroup.getChildren().add(exitButton);
-        anchorPane.getChildren().add(buttonGroup);
-
+        subPane.getChildren().add(exitButton);
+        mainPane.getChildren().add(subPane);
 
         playStage.heightProperty().addListener((observable, oldValue, newValue) -> {
             double scaleFactor = newValue.doubleValue() / 1080;
@@ -74,13 +64,38 @@ public final class SlidePlay {
             exitButton.setFont(Font.font(30 * scaleFactor));
             exitButton.setLayoutX(1375 * scaleFactor);
         });
-        playScene = new Scene(anchorPane);
+
         playStage.setScene(playScene);
-        PauseTransition pause = new PauseTransition(Duration.seconds(2));
-        pause.play();
         playStage.show();
     }
 
+    //TODO 为主窗口设计,供外部调用
+//    public static void playPicture(){
+//        initStyle();
+//
+//
+//    }
+    private static int i = 0;
+    public static void playPicture(){
+        initStyle();
+        FlowPane flowPane = PictureShower.getThumbnails();
+        mainPane.getChildren().add(imageView);
+        ArrayList<File> arrayList = new ArrayList<>();
+        for(Node node : flowPane.getChildren()){
+            arrayList.add(((Thumbnail)node).getImageFile());
+        }
+        Timeline delay = new Timeline(new KeyFrame(Duration.seconds(2),event->{
+            Image image = new Image(arrayList.get(i).toString());
+            imageView.setImage(image);
+            i++;
+        }));
+        delay.setCycleCount(arrayList.size());
+        delay.play();
+
+    }
+
+    private static void sleep(int i) {
+    }
 
 
 }
