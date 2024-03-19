6a8362e98b8cc9100f22b375f75543ff885b4d4b
Jack299q <1625621154@qq.com>
2024-03-19 13:46:51 +0800

    放大 缩小雏形

--- src/main/java/top/ithaic/shower/SlideShower/SlideController.java
+++ src/main/java/top/ithaic/shower/SlideShower/SlideController.java
@@ -7,15 +7,19 @@ import javafx.scene.layout.Pane;
 
 public class SlideController {
     @FXML
-    private Pane pictureShower;
+    private Button shrinkPicture;
+    @FXML
+    private Button amplifyPicture;
     @FXML
     private Button lastPicture;
     @FXML
     private Button nextPicture;
+    @FXML
+    private Pane pictureShower;
 
     @FXML
     private void initialize(){
-        new SlideShower(pictureShower,lastPicture,nextPicture);
+        new SlideShower(pictureShower,lastPicture,nextPicture,amplifyPicture,shrinkPicture);
         new SlideListener(pictureShower);
     }
 
--- src/main/java/top/ithaic/shower/SlideShower/SlideShower.java
+++ src/main/java/top/ithaic/shower/SlideShower/SlideShower.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -12,6 +13,9 @@ import javafx.scene.layout.Pane;
 
 
 public class SlideShower{
+    private static double factor;
+    private static Button shrinkPicture;
+    private static Button amplifyPicture;
     private static Button lastPicture;
     private static Button nextPicture;
     private static Pane pictureShower;
@@ -20,10 +24,13 @@ public class SlideShower{
 
     public SlideShower(){}
 
-    public SlideShower(Pane pictureShower,Button lastPicture,Button nextPicture){
+    public SlideShower(Pane pictureShower,Button lastPicture,Button nextPicture,Button amplifyPicture,Button shrinkPicture){
         SlideShower.pictureShower = pictureShower;
         SlideShower.lastPicture = lastPicture;
         SlideShower.nextPicture = nextPicture;
+        SlideShower.amplifyPicture = amplifyPicture;
+        SlideShower.shrinkPicture = shrinkPicture;
+        SlideShower.factor = 0;
         showPicture();
     }
 
@@ -40,8 +47,8 @@ public class SlideShower{
         canvas.heightProperty().bind(pictureShower.heightProperty());
 
         //画布监听
-        canvas.widthProperty().addListener(((observableValue, number, t1) -> drawPicture(canvas, image)));
-        canvas.heightProperty().addListener(((observableValue, number, t1) -> drawPicture(canvas, image)));
+        canvas.widthProperty().addListener(((observableValue, number, t1) -> drawPicture(canvas, image,1+factor)));
+        canvas.heightProperty().addListener(((observableValue, number, t1) -> drawPicture(canvas, image,1+factor)));
 
         //按钮监听
         if(SlideFileManager.getPictures().length==1){
@@ -49,17 +56,15 @@ public class SlideShower{
             nextPicture.setVisible(false);
         }
         else{
-            lastPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> {
-                lastPicture(canvas);
-            });
-            nextPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> {
-                nextPicture(canvas);
-            });
+            lastPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> lastPicture(canvas));
+            nextPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> nextPicture(canvas));
             pictureShower.getParent().addEventFilter(KeyEvent.KEY_PRESSED,keyEvent -> {
                 if(keyEvent.getCode() == KeyCode.LEFT)lastPicture(canvas);
                 if(keyEvent.getCode() == KeyCode.RIGHT)nextPicture(canvas);
             });
         }
+        amplifyPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> amplifyPicture(factor+=0.1));
+        shrinkPicture.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseEvent -> shrinkPicture(factor-=0.1));
 
 
         pictureShower.getChildren().add(canvas);
@@ -67,21 +72,23 @@ public class SlideShower{
 
     private void lastPicture(Canvas canvas){
         if(SlideFileManager.getCurrentIndex()>0) {
+            factor = 0;
             image = new Image(SlideFileManager.getPictures()[SlideFileManager.getCurrentIndex()-1].toURI().toString());
             SlideFileManager.setCurrentIndex(SlideFileManager.getCurrentIndex()-1);
-            drawPicture(canvas,image);
+            drawPicture(canvas,image,1+factor);
         }
     }
 
     private void nextPicture(Canvas canvas){
         if(SlideFileManager.getCurrentIndex()<SlideFileManager.getPictures().length-1) {
+            factor = 0;
             image = new Image(SlideFileManager.getPictures()[SlideFileManager.getCurrentIndex()+1].toURI().toString());
             SlideFileManager.setCurrentIndex(SlideFileManager.getCurrentIndex()+1);
-            drawPicture(canvas,image);
+            drawPicture(canvas,image,1);
         }
     }
 
-    private void drawPicture(Canvas canvas,Image image){
+    private void drawPicture(Canvas canvas,Image image,double factor){
         double canvasWidth = canvas.getWidth();
         double canvasHeight = canvas.getHeight();
 
@@ -94,17 +101,24 @@ public class SlideShower{
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvasWidth, canvasHeight);
-        gc.drawImage(image, 0, 0, imageWidth, imageHeight,
+        gc.drawImage(image, (imageWidth*factor-imageWidth)/2,(imageHeight*factor-imageHeight)/2 ,
+                imageWidth-(imageWidth*factor-imageWidth)/2, imageHeight-(imageHeight*factor-imageHeight)/2,
                 (canvasWidth - scaledWidth) / 2, (canvasHeight - scaledHeight) / 2,
                 scaledWidth, scaledHeight);
     }
 
     public void drawPicture(){
         image = new Image(SlideFileManager.getPictures()[SlideFileManager.getCurrentIndex()].toURI().toString());
-        this.drawPicture(canvas,image);
+        this.drawPicture(canvas,image,1);
     }
-    
 
+    private void amplifyPicture(double factor){
+        drawPicture(canvas,image,1+factor);
+    }
+
+    private void shrinkPicture(double factor){
+        drawPicture(canvas,image,1+factor);
+    }
 
 }
 
